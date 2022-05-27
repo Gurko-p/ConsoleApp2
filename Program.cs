5801a11c935806c2d84e03eab88361b55c3b41b0
Pavel <gurko-p@mail.ru>
2022-05-27 16:49:24 +0300

    working version

--- Program.cs
+++ Program.cs
@@ -42,19 +42,19 @@ namespace ConsoleApp2
         {
             Dictionary<int, List<int>> lgots = new Dictionary<int, List<int>>
             {
-                [1] = new List<int> { 1, 2 },
+                [1] = new List<int> { 2 },
                 [2] = new List<int> { 2 },
             };
 
-
-
             List<Abiturient> ab = new List<Abiturient>
             {
-                new Abiturient { Id = 1, FIO = "Иванов", Sex = 1, Lgota = 1, Result = 180, Specialities = new int[]{ 2,1} }, // 2
-                new Abiturient { Id = 2, FIO = "Петров", Sex = 1, Result = 360, Specialities = new int[]{ 1,0 } }, // 2
-                new Abiturient { Id = 3, FIO = "Сидоров", Lgota = 2, Sex = 1, Result = 180, Specialities = new int[]{ 1, 2 } }, // 2
-                new Abiturient { Id = 4, FIO = "Смирнов", Sex = 1, Result = 237, Specialities = new int[]{ 1,2 } }, // 2
-                new Abiturient { Id = 5, FIO = "Хващевский", Sex = 1, Result = 250, Specialities = new int[]{ 1,2 } }, // 2
+                new Abiturient { Id = 1, FIO = "Иванов", Sex = 1, Lgota = 1, Result = 180, Specialities = new int[]{ 2, 1 } }, // 2
+                new Abiturient { Id = 2, FIO = "Петров", Sex = 1, Lgota = 1, Result = 360, Specialities = new int[]{ 1, 0 } }, // 2
+                new Abiturient { Id = 3, FIO = "Сидоров", Sex = 1, Lgota = 2, Result = 180, Specialities = new int[]{ 1, 2 } }, // 2
+                new Abiturient { Id = 4, FIO = "Смирнов", Sex = 1, Lgota = 1, Result = 237, Specialities = new int[]{ 1, 2 } }, // 2
+                new Abiturient { Id = 5, FIO = "Хващевский", Sex = 1, Lgota = 2, Result = 250, Specialities = new int[]{ 1, 0 } }, // 2
+                new Abiturient { Id = 6, FIO = "Сидоров", Sex = 1, Lgota = 0, Result = 180, Specialities = new int[]{ 1, 2 } }, // 2
+                new Abiturient { Id = 7, FIO = "Сидоров", Sex = 1, Lgota = 0, Result = 180, Specialities = new int[]{ 1, 2 } }, // 2
             };
 
             ab.Sort();
@@ -63,7 +63,7 @@ namespace ConsoleApp2
             List<Abiturient> failed = new List<Abiturient>();
             List<int> success = new List<int>();
 
-            List<Abiturient> sp1 = new List<Abiturient>(2);
+            List<Abiturient> sp1 = new List<Abiturient>(3);
             List<Abiturient> sp2 = new List<Abiturient>(2);
             Dictionary<string, List<Abiturient>> dict = new Dictionary<string, List<Abiturient>>()
             {
@@ -71,50 +71,43 @@ namespace ConsoleApp2
                 ["sp2"] = sp2,
             };
 
+            List<Abiturient> lgota = ab.Where(a => a.Lgota == 1).ToList();
+            Console.WriteLine(lgota.Count);
 
             for (int i = 0; i < 2; i++)
             {
-                for (int j = 0; j < ab.Count; j++)
+                for (int j = 0; j < lgota.Count; j++)
                 {
-                    int sp = ab[j].Specialities[i];
+                    int sp = lgota[j].Specialities[i];
                     Dictionary<string, List<Abiturient>> abiturients = dict;
-                    if (sp != 0)
+                    if (sp != 0 && lgots[sp].Contains(lgota[j].Specialities[i]))
                     {
-                        if (abiturients["sp" + sp].Count < abiturients["sp" + sp].Capacity)
+                        if (abiturients["sp" + sp].Count < Math.Round(abiturients["sp" + sp].Capacity * 0.3))
                         {
-                            if (!success.Contains(ab[j].Id))
+                            if (!success.Contains(lgota[j].Id))
                             {
-                                abiturients["sp" + sp].Add(ab[j]);
-                                success.Add(ab[j].Id);
+                                abiturients["sp" + sp].Add(lgota[j]);
+                                success.Add(lgota[j].Id);
                             }
                         }
                         else
                         {
                             int minResSp = abiturients["sp" + sp].Min(a => a.Result);
-                            if (ab[j].Result > minResSp && !success.Contains(ab[j].Id))
+                            if (lgota[j].Result > minResSp && !success.Contains(lgota[j].Id))
                             {
                                 Abiturient a = abiturients["sp" + sp].Where(a => a.Result == minResSp).LastOrDefault();
                                 abiturients["sp" + sp].Remove(a);
-                                abiturients["sp" + sp].Add(ab[j]);
+                                abiturients["sp" + sp].Add(lgota[j]);
                                 success.Remove(a.Id);
-                                success.Add(ab[j].Id);
+                                success.Add(lgota[j].Id);
                             }
 
-                        }
-                        if (i + 1 >= ab[j].Specialities.Length && !success.Contains(ab[j].Id) && !failed.Contains(ab[j]))
-                        {
-                            failed.Add(ab[j]);
-                        }
-                    }
-                    else
-                    {
-                        if (!success.Contains(ab[j].Id) && !failed.Contains(ab[j]))
-                        {
-                            failed.Add(ab[j]);
                         }
                     }
                 }
             }
+
+            dict["sp1"].Add(new Abiturient { Id = 99999, FIO = "Сидоров", Sex = 1, Lgota = 0, Result = 500, Specialities = new int[] { 1, 2 } });
 
             Console.WriteLine("Spec1");
             for (int i = 0; i < dict["sp1"].Count; i++)
@@ -143,8 +136,8 @@ namespace ConsoleApp2
                 Console.WriteLine(failed[i].Id);
             }
 
-
             Console.ReadKey();
         }
     }
 }
+
